e3dd33052ba1dd5758ba811e05cfb283f1518bf4
Andres Namm <anamm@adaptavist.com>
2020-12-06 15:36:52 +0200

    Email changes

--- src/test_scraping_results.py
+++ src/test_scraping_results.py
@@ -5,6 +5,7 @@ from typing import Final
 from util_functions.athena_queries import AthenaQueries
 import json
 import pandas as pd
+from datetime import date
 
 #sd
 WRITE_TO_LOCAL_CSV=False
@@ -18,6 +19,8 @@ logger = logging.getLogger(__name__)
 q = AthenaQueries()
 
 
+all_emails=["andres.namm.001@gmail.com","sigridmalinen@gmail.com", "jyrimalinen1@gmail.com", "annemalinen27@gmail.com", "jaan911@gmail.com", "rauno.naksi@gmail.com", "renartupits@gmail.com", "kyllike@namm.ee","andres.namm@namm.ee","mart.randala@gmail.com"]
+main_man=["andres.namm.001@gmail.com"]
 alarm_basis = {'Rakvere-1': {'cnt': 9}, 'Rakvere-2': {'cnt': 28}, 'Rakvere-3': {'cnt': 19}, 'Rakvere-4': {'cnt': 2}, 'Tallinn-1': {'cnt': 719}, 'Tallinn-2': {'cnt': 1399}, 'Tallinn-3': {'cnt': 1043}, 'Tallinn-4': {'cnt': 409}, 'Tallinn-5': {'cnt': 70}, 'Tartu-1': {'cnt': 157}, 'Tartu-2': {'cnt': 334}, 'Tartu-3': {'cnt': 291}, 'Tartu-4': {'cnt': 109}, 'Tartu-5': {'cnt': 9}}
 
 def perform_tests():
@@ -46,9 +49,14 @@ def perform_tests():
                 query3= f"""SELECT concat('https://www.kv.ee/',id) as url,* FROM "dbt"."active_sale_highest_price_drops" where city = '{city}' order by diff_perc desc limit 20;"""
                 df = q.athena_query(query3)
                 full_df = pd.concat([full_df,df])
-            data=full_df.to_html()  
-            send_info_email(data, 'Suurimad langetajad Tartu,Rakvere,Tallinn',["andres.namm.001@gmail.com","sigridmalinen@gmail.com", "jyrimalinen1@gmail.com", "annemalinen27@gmail.com", "jaan911@gmail.com", "rauno.naksi@gmail.com", "renartupits@gmail.com", "kyllike@namm.ee","andres.namm@namm.ee"])
+            data=full_df.to_html() 
 
+            if date.today().weekday() == 6:
+                send_info_email(data, 'Suurimad langetajad Tartu,Rakvere,Tallinn',all_emails)
+            else:
+                send_info_email(data, 'Suurimad langetajad Tartu,Rakvere,Tallinn',main_man)
+ 
+            
 def lambda_handler(event, context):
     perform_tests()
 
